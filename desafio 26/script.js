533b78daf8335a31097a69cf7bc5cba0ae84bee4
Bubblesu <lanaramos221@gmail.com>
2023-07-05 01:01:22 -0300

    solução desafio de exercicios

---
+++ desafio 26/script.js
@@ -0,0 +1,13 @@
+var quantidade = 0;
+function add (){
+    quantidade = quantidade + 1;
+    document.getElementById('quantity').innerHTML = quantidade;
+
+}
+
+function sub (){
+    quantidade = quantidade - 1;
+    document.getElementById('quantity').innerHTML = quantidade;
+
+}
+
